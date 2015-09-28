945a16203b4a35523989ece8da2f4998eed8420c
Rich Donnellan <rich.donnellan@gmail.com>
2015-09-27 21:57:19 -0700

    Update getWinner functionality.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,13 +35,22 @@ function getComputerMove(move) {
     return move || randomPlay();
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
+function getWinner(playerMove, computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
-    return winner;
+    if (playerMove === computerMove) {
+        return 'tie';
+    } else if (
+            playerMove === 'rock' && computerMove === 'scissors' ||
+            playerMove === 'scissors' && computerMove === 'paper' ||
+            playerMove === 'paper' && computerMove === 'rock'
+        ) {
+        return 'player';
+    } else {
+        return 'computer';
+    }
 }
 
 function playToFive() {
